23bd8d597e26fabefe5ad3d7c8fbf9573e5f9eaf
oyasumi02 <jaguilar1999@live.com>
2025-08-03 03:52:30 -0500

    Added cmd arg for countdown

--- src/args.c
+++ src/args.c
@@ -9,5 +9,10 @@ void ARGS_Parse(int argc, char *argv[], bool *isCounting, PROGRAM_STATE *program
         *program_state = PROGRAM_TIMER_RUNNING;
         *timer_mode = TIMER_MODE_STOPWATCH;
         *timer_state = TIMER_STATE_RUNNING;
+    } else if (strcmp(argv[1], "countdown") == 0 || strcmp(argv[1], "ct") == 0 || strcmp(argv[1], "c") == 0) {
+        *isCounting = true;
+        *program_state = PROGRAM_UI_CONFIGURE_TIMER;
+        *timer_mode = TIMER_MODE_COUNTDOWN;
+        *timer_state = TIMER_STATE_RUNNING;
     }
 }
